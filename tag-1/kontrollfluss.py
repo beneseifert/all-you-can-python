b8f26d088ad2f90aac329b616d3c82c652514c89
Benedikt Seifert <bene.seifert@gmail.com>
2019-03-06 15:14:17 +0100

    Add solutions for day 1 and day 2

--- tag-1/kontrollfluss.py
+++ tag-1/kontrollfluss.py
@@ -11,8 +11,16 @@ except ValueError:
 
 if geratene_zahl == geheime_zahl:
     print("Richtig!")
+elif geratene_zahl < geheime_zahl:
+    print("Zu klein")
 else:
-    print("Falsch!")
+    print("Zu groß")
+
+if geratene_zahl < geheime_zahl - 10:
+    print("Viel zu niedrig")
+
+if geratene_zahl > geheime_zahl + 10:
+    print("Viel zu hoch")
 
 # Aufgabe:
 # 1. Erweitere den Code so, dass auch ausgegeben wird, ob man zu hoch, oder zu niedrig geraten hat
--- tag-2/schleifen.py
+++ tag-2/schleifen.py
@@ -2,19 +2,27 @@ import random
 
 geheime_zahl = random.randint(0, 99)
 geratene_zahl = 0
+anzahl_versuche = 0
 
-print("Gib eine Zahl ein")
-try:
-    geratene_zahl = int(input())
-except ValueError:
-    print("Gib bitte eine Zahl ein!")
+while True:
+    if anzahl_versuche >= 10:
+        break
 
-if geratene_zahl < geheime_zahl:
-    print("Die gesuchte Zahl ist größer!")
-elif geratene_zahl > geheime_zahl:
-    print("Die gesuchte Zahl ist kleiner!")
-else:
-    print("Richtig!")
+    print("Gib eine Zahl ein")
+    try:
+        geratene_zahl = int(input())
+    except ValueError:
+        print("Gib bitte eine Zahl ein!")
+
+    if geratene_zahl < geheime_zahl:
+        print("Die gesuchte Zahl ist größer!")
+    elif geratene_zahl > geheime_zahl:
+        print("Die gesuchte Zahl ist kleiner!")
+    else:
+        print("Richtig!")
+        break
+
+    anzahl_versuche += 1
 
 # Aufgabe:
 # Erweitere den Code von Tag 1 so, dass das Spiel nicht ständig neu geladen werden muss
